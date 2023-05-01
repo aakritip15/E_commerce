76a35090df86d4bd30077d2337307339165a0e75
Shashinoor13 <shashinoorghimire13@gmail.com>
2023-05-01 20:09:12 +0545

    Improving Ui

--- lib/Screens/MyOrders.dart
+++ lib/Screens/MyOrders.dart
@@ -42,7 +42,7 @@ class _OrdersState extends State<Orders> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[50],
+      backgroundColor: Color.fromARGB(255, 250, 249, 244),
       appBar: APPBAR(
         context,
       ),
@@ -62,74 +62,26 @@ class _OrdersState extends State<Orders> {
             } else {
               List<Items> products = snapshot.data!;
 
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: products.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
+              return Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: products.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey,
-                                blurRadius: 2,
-                                spreadRadius: 0.3,
-                                offset: Offset(0, 1),
-                              ),
-                            ]),
-                        child: ListTile(
-                            onTap: () {
-                              print(products[index].ProductName);
-                            },
-                            title: Text(products[index].ProductName!),
-                            subtitle: Text(
-                                'Status:${products[index].ProductStatus!}'),
-                            trailing: SizedBox(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey,
-                                      blurRadius: 2,
-                                      spreadRadius: 0.3,
-                                      offset: Offset(0, 1),
-                                    ),
-                                  ],
-                                ),
-                                child: IconButton(
-                                  icon: Icon(
-                                    Icons.delete,
-                                    color: kRed,
-                                  ),
-                                  onPressed: () {
-                                    FirebaseFirestore.instance
-                                        .collection('Orders')
-                                        .where('ProductName',
-                                            isEqualTo:
-                                                products[index].ProductName)
-                                        .get()
-                                        .then((value) {
-                                      for (var element in value.docs) {
-                                        FirebaseFirestore.instance
-                                            .collection('Orders')
-                                            .doc(element.id)
-                                            .delete();
-                                      }
-                                      products.removeAt(index);
-                                      setState(
-                                          () {}); // This line will trigger a rebuild of the widget tree
-                                    });
-                                  },
-                                ),
-                              ),
-                            )),
-                      ));
-                },
+                          ),
+                          child: orderTile(
+                            products[index].ProductName,
+                            products[index].ProductSellerID,
+                            products[index].ProductPrice,
+                            products[index].ProductStatus,
+                          ),
+                        ));
+                  },
+                ),
               );
             }
           },
@@ -138,3 +90,153 @@ class _OrdersState extends State<Orders> {
     );
   }
 }
+
+orderTile(
+    String? ProductName, String? SellerName, String? Price, String? Status) {
+  // String? ProductName = "Samsung Phone";
+  // String? SellerName = "Shashinoor Ghimire";
+  // String? Price = "1000";
+  // String? Status = "Pending";
+  return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 223, 232, 224),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            ProductName!,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Seller:$SellerName',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(
+                'Price: $Price!',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text(
+                'Status: $Status!',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 0, 0, 0)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 244, 249, 103)),
+                  padding:
+                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                ),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text('View Details',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w300,
+                        )),
+                    Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 255, 255, 255)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 228, 69, 45)),
+                  padding:
+                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                ),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text('Cancel Order'),
+                    Icon(Icons.cancel_outlined),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ));
+}
+
+
+  // child: ListTile(
+  //                           onTap: () {
+  //                             print(products[index].ProductName);
+  //                           },
+  //                           title: Text(products[index].ProductName!),
+  //                           subtitle: Text(
+  //                               'Status:${products[index].ProductStatus!}'),
+  //                           trailing: SizedBox(
+  //                             child: Container(
+  //                               decoration: BoxDecoration(
+  //                                 color: Colors.white,
+  //                                 borderRadius: BorderRadius.circular(10),
+  //                                 boxShadow: [
+  //                                   BoxShadow(
+  //                                     color: Colors.grey,
+  //                                     blurRadius: 2,
+  //                                     spreadRadius: 0.3,
+  //                                     offset: Offset(0, 1),
+  //                                   ),
+  //                                 ],
+  //                               ),
+  //                               child: IconButton(
+  //                                 icon: Icon(
+  //                                   Icons.delete,
+  //                                   color: kRed,
+  //                                 ),
+  //                                 onPressed: () {
+  //                                   FirebaseFirestore.instance
+  //                                       .collection('Orders')
+  //                                       .where('ProductName',
+  //                                           isEqualTo:
+  //                                               products[index].ProductName)
+  //                                       .get()
+  //                                       .then((value) {
+  //                                     for (var element in value.docs) {
+  //                                       FirebaseFirestore.instance
+  //                                           .collection('Orders')
+  //                                           .doc(element.id)
+  //                                           .delete();
+  //                                     }
+  //                                     products.removeAt(index);
+  //                                     setState(
+  //                                         () {}); // This line will trigger a rebuild of the widget tree
+  //                                   });
+  //                                 },
+  //                               ),
+  //                             ),
+  //                           )),
