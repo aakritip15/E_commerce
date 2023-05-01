5d8263b0e8f71410284fe41b7a3257e3106110ce
Shashinoor13 <shashinoorghimire13@gmail.com>
2023-05-01 20:24:30 +0545

    Functionality to the Page

--- lib/Screens/MyOrders.dart
+++ lib/Screens/MyOrders.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
 import 'dart:async';
 
@@ -7,6 +7,7 @@ import 'package:app_1/models/appbar.dart';
 import 'package:app_1/models/nav.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../models/Orders.dart';
 
@@ -39,8 +40,130 @@ class _OrdersState extends State<Orders> {
     return products;
   }
 
+  bool shouldRefresh = false;
+
   @override
   Widget build(BuildContext context) {
+    Container orderTile(String? ProductName, String? SellerName, String? Price,
+        String? Status, List<Items> products, BuildContext context) {
+      // String? ProductName = "Samsung Phone";
+      // String? SellerName = "Shashinoor Ghimire";
+      // String? Price = "1000";
+      // String? Status = "Pending";
+      return Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 223, 232, 224),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              ProductName!,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Seller:$SellerName',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Text(
+                  'Price: $Price!',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Status: $Status!',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 0, 0, 0)),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 244, 249, 103)),
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Text('View Details',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w300,
+                          )),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 255, 255, 255)),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 228, 69, 45)),
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                  ),
+                  onPressed: () {
+                    FirebaseFirestore.instance
+                        .collection('Orders')
+                        .where('ProductName', isEqualTo: ProductName)
+                        .get()
+                        .then((value) {
+                      value.docs.forEach((element) {
+                        FirebaseFirestore.instance
+                            .collection('Orders')
+                            .doc(element.id)
+                            .delete();
+                      });
+                      products.removeWhere((element) =>
+                          element.ProductName == ProductName &&
+                          element.ProductSellerID == SellerName &&
+                          element.ProductPrice == Price &&
+                          element.ProductStatus == Status);
+                      setState(() {});
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Text('Cancel Order'),
+                      Icon(Icons.cancel_outlined),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 250, 249, 244),
       appBar: APPBAR(
@@ -78,6 +201,8 @@ class _OrdersState extends State<Orders> {
                             products[index].ProductSellerID,
                             products[index].ProductPrice,
                             products[index].ProductStatus,
+                            products,
+                            context,
                           ),
                         ));
                   },
@@ -91,105 +216,126 @@ class _OrdersState extends State<Orders> {
   }
 }
 
-orderTile(
-    String? ProductName, String? SellerName, String? Price, String? Status) {
-  // String? ProductName = "Samsung Phone";
-  // String? SellerName = "Shashinoor Ghimire";
-  // String? Price = "1000";
-  // String? Status = "Pending";
-  return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 223, 232, 224),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            ProductName!,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Seller:$SellerName',
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Text(
-                'Price: $Price!',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              SizedBox(width: 10),
-              Text(
-                'Status: $Status!',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 0, 0, 0)),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 244, 249, 103)),
-                  padding:
-                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                ),
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    Text('View Details',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300,
-                        )),
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-              ),
-              SizedBox(width: 10),
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 255, 255, 255)),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 228, 69, 45)),
-                  padding:
-                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                ),
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    Text('Cancel Order'),
-                    Icon(Icons.cancel_outlined),
-                  ],
-                ),
-              ),
-            ],
-          )
-        ],
-      ));
-}
+// Container orderTile(String? ProductName, String? SellerName, String? Price,
+//     String? Status, List<Items> products, BuildContext context) {
+//   // String? ProductName = "Samsung Phone";
+//   // String? SellerName = "Shashinoor Ghimire";
+//   // String? Price = "1000";
+//   // String? Status = "Pending";
+//   return Container(
+//     margin: EdgeInsets.all(10),
+//     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+//     decoration: BoxDecoration(
+//       color: Color.fromARGB(255, 223, 232, 224),
+//       borderRadius: BorderRadius.circular(15),
+//     ),
+//     child: Column(
+//       mainAxisAlignment: MainAxisAlignment.start,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Text(
+//           ProductName!,
+//           style: TextStyle(
+//             fontSize: 20,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         SizedBox(height: 10),
+//         Text(
+//           'Seller:$SellerName',
+//           style: TextStyle(
+//             fontSize: 15,
+//             fontWeight: FontWeight.w300,
+//           ),
+//         ),
+//         SizedBox(height: 10),
+//         Row(
+//           children: [
+//             Text(
+//               'Price: $Price!',
+//               style: TextStyle(
+//                 fontSize: 15,
+//                 fontWeight: FontWeight.w300,
+//               ),
+//             ),
+//             SizedBox(width: 10),
+//             Text(
+//               'Status: $Status!',
+//               style: TextStyle(
+//                 fontSize: 15,
+//                 fontWeight: FontWeight.w300,
+//               ),
+//             ),
+//           ],
+//         ),
+//         SizedBox(height: 20),
+//         Row(
+//           children: [
+//             TextButton(
+//               style: ButtonStyle(
+//                 foregroundColor: MaterialStateProperty.all<Color>(
+//                     Color.fromARGB(255, 0, 0, 0)),
+//                 backgroundColor: MaterialStateProperty.all<Color>(
+//                     Color.fromARGB(255, 244, 249, 103)),
+//                 padding:
+//                     MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+//               ),
+//               onPressed: () {},
+//               child: Row(
+//                 children: [
+//                   Text('View Details',
+//                       style: TextStyle(
+//                         fontSize: 15,
+//                         fontWeight: FontWeight.w300,
+//                       )),
+//                   Icon(Icons.arrow_forward_ios),
+//                 ],
+//               ),
+//             ),
+//             SizedBox(width: 10),
+//             TextButton(
+//               style: ButtonStyle(
+//                 foregroundColor: MaterialStateProperty.all<Color>(
+//                     Color.fromARGB(255, 255, 255, 255)),
+//                 backgroundColor: MaterialStateProperty.all<Color>(
+//                     Color.fromARGB(255, 228, 69, 45)),
+//                 padding:
+//                     MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+//               ),
+//               onPressed: () {
+//                 FirebaseFirestore.instance
+//                     .collection('Orders')
+//                     .where('ProductName', isEqualTo: ProductName)
+//                     .get()
+//                     .then((value) {
+//                   value.docs.forEach((element) {
+//                     FirebaseFirestore.instance
+//                         .collection('Orders')
+//                         .doc(element.id)
+//                         .delete();
+//                   });
+//                   products.removeWhere((element) =>
+//                       element.ProductName == ProductName &&
+//                       element.ProductSellerID == SellerName &&
+//                       element.ProductPrice == Price &&
+//                       element.ProductStatus == Status);
+//                 });
+//               },
+//               child: Row(
+//                 children: [
+//                   Text('Cancel Order'),
+//                   Icon(Icons.cancel_outlined),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         )
+//       ],
+//     ),
+//   );
+// }
+
+
 
 
   // child: ListTile(
