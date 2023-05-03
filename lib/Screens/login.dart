45451c10b432e7d757cf8deee52e432821726cea
Shashinoor13 <shashinoorghimire13@gmail.com>
2023-05-03 16:12:02 +0545

    Forgot Password Added  and terms and Conditions

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -2,6 +2,7 @@
 
 import 'package:app_1/Screens/landing_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
@@ -21,6 +22,69 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController ResetemailController = TextEditingController();
+  IconData visibility_off = EvaIcons.eyeOffOutline;
+  //Update with actual Terms and Condition;
+  String TandC =
+      "A Terms and Conditions agreement acts as a legal contract between you (the company) and the user. Its where you maintain your rights to exclude users from your app in the event that they abuse your website/app, set out the rules for using your service and note other important details and disclaimers.Having a Terms and Conditions agreement is completely optional. No laws require you to have one. Not even the super-strict and wide-reaching General Data Protection Regulation (GDPR).Your Terms and Conditions agreement will be uniquely yours. While some clauses are standard and commonly seen in pretty much every Terms and Conditions agreement, it's up to you to set the rules and guidelines that the user must agree to.Terms and Conditions agreements are also known as Terms of Service or Terms of Use agreements. These terms are interchangeable, practically speaking.";
+
+  void forgotPassWord() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Forgot Password"),
+          content: Container(
+            height: 90,
+            child: Column(
+              children: [
+                Text("Email:"),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    width: 250,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                        controller: ResetemailController,
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(10),
+                        ))),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (ResetemailController.text.trim().isNotEmpty) {
+                  Navigator.pop(context);
+                  FirebaseAuth.instance.sendPasswordResetEmail(
+                      email: ResetemailController.text.trim());
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Reset Password Email Sent!"),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Enter Email!"),
+                    ),
+                  );
+                }
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   void loginAttempt() async {
     String email = emailController.text.trim();
@@ -134,8 +198,14 @@ class _LoginState extends State<Login> {
                   ],
                 ),
               ),
+              TextButton(
+                onPressed: () {
+                  forgotPassWord();
+                },
+                child: Text('Forgot Password?'),
+              ),
               const SizedBox(
-                height: 30.0,
+                height: 10.0,
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -171,24 +241,66 @@ class _LoginState extends State<Login> {
                 ),
               ),
               SizedBox(
-                height: 50,
+                height: 30,
               ),
-              RichText(
-                text: TextSpan(
-                  text: "By signing up you will be accepting our ",
-                  style: TextStyle(color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'Terms and Conditions',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            decoration: TextDecoration.underline),
-                        recognizer: TapGestureRecognizer()..onTap = () {}),
-                    const TextSpan(
-                      text: '.',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: "By signing up you will be accepting our ",
+                    style: TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Terms and Conditions',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return (Column(
+                                    children: [
+                                      SizedBox(
+                                        height: 30,
+                                      ),
+                                      Text(
+                                        'Terms and Conditions',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      SizedBox(
+                                        height: 300,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child: Padding(
+                                          padding: EdgeInsets.all(10.0),
+                                          child: Expanded(
+                                            child: Text(
+                                              TandC,
+                                              style: TextStyle(
+                                                  fontSize: 12,
+                                                  color: Colors.black),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ));
+                                },
+                              );
+                            }),
+                      const TextSpan(
+                        text: '.',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
